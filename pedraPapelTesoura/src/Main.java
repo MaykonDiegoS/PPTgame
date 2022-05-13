c374924ce524108e3d27071fa5c1398a1599912d
Andre Schmitt <aschmitt4@furb.br>
2022-05-13 18:18:00 -0300

    ...

---
+++ pedraPapelTesoura/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner();
+
+        System.out.println("Informe palavra da forca: ");
+        String palavra = in.nextLine();
+        char[] letrasCorretas = new char[palavra.length()];
+        char[] letrasErradas = new char[36];
+
+        System.out.println("Informe uma letra: ");
+        char letraInf = new.char,
+    }
+}
+
